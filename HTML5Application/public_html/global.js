b5efddcf0c6f76fb8fb415771a32fbe8fa4dded8
Peter Henderson <peter.henderson@capgemini.com>
2016-10-17 23:01:21 +0100

    New Global file for variables and functions

---
+++ HTML5Application/public_html/global.js
@@ -0,0 +1,22 @@
+/* 
+ * This File is intented to contain al global/final variables as well 
+ * as any utility functions that may be used repeatedly thoughout the game
+ *Author: Peter Henderson
+ *Date: 17/10/2016
+ *Contribution Log: Name/Date/Description
+ *
+ *Pete/17/10/2015/Created File, added ship definition Final Variables
+ *
+ * 
+ */
+
+
+//ship variables
+
+var CARRIER_LEN = 5;
+var BATTLESHIP_LEN = 4;
+var CRUISER_LEN = 3;
+var SUBMARINE_LEN = 3;
+var DESTROYER_LEN = 2;
+
+
