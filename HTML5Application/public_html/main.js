bda882e7b288967ec60fd6b43726e1aa37f82024
Peter Henderson <peter.henderson@capgemini.com>
2016-10-18 22:43:20 +0100

    Main.js added as discussed. currently just a template with comment placeholders for functionality

---
+++ HTML5Application/public_html/main.js
@@ -0,0 +1,20 @@
+/* 
+ * This file will act as the main code. 
+ * This should be the only place where code (besides variable declaration) happens outside of a function
+ * Author: Peter Henderson
+ * Date: 18/10/2016
+ * Contribution Log: Name/Date/Description
+ *
+ */
+
+//MAIN MENU GOES HERE 
+
+//GAME SETUP GOES HERE - (POST HACK)
+
+//SHIP POSITIONING GOES HERE 
+
+//GAMEPLAY GOES HERE 
+
+//POST GAME GOES HERE
+
+
