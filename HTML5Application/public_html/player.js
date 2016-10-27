faf9002cf5bd0dbfd04a9f8bf18dc836eed8754d
Peter Henderson <peter.henderson@capgemini.com>
2016-10-27 23:25:27 +0100

    Added grid, build move list and buildmovelist function

--- HTML5Application/public_html/player.js
+++ HTML5Application/public_html/player.js
@@ -1,17 +1,35 @@
 /*
  * Created by Charlotte Roche on 20/10/2016.
- * Only holds details about the player now. can expand if needed for the MVP
+ *  *Contribution Log: Name/Date/Description
+ *  Peter Henderson/27/10/2016/Added grid, build move list and buildmovelist function
  */
 
 
-function Player(name){
-	this.name = name;
+function player(name, xSize, ySize) {
+    this.name = name;
+    this.moveList = this.buildMoveList(xSize, ySize);
+    this.grid = new Grid(xSize, ySize);
+
+
+
 }
 
-Player.prototype = {
-	constructor:Player,
-	getName:function(){
-		return this.name;
-	}
+player.prototype = {
+    constructor: player,
+    getName: function () {
+        return this.name;
+    },
+    makePlayerMove: function (){
+        
+    },
+    buildMoveList: function (xSize, ySize) {
+        var moveList = [];
+        for (i = 0; i < ySize; i++) {
+            for (j = 0; j < xSize; j++) {
+                moveList.push(i + ',' + j);
+            }
+        }
+        return moveList;
+    }
 };
 
