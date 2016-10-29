fd33fb6072f29632d1a8608b3a6f2ef849136140
Abby Walker <walkera3@aston.ac.uk>
2016-10-29 01:16:36 +0100

    A

--- HTML5Application/public_html/player.js
+++ HTML5Application/public_html/player.js
@@ -32,9 +32,36 @@ player.prototype = {
         return moveList;
     },
     drawGrid: function () { //draw the players grid containing their ships
-       this.grid; //this contains locations of all of the players ships (and if they have been hit)
-       AI.movelist; //This contains everymove the AI is yet to make.
+        var squareSize = 50;
+        var gameBoardContainer = document.getElementById("gameboard");
+        // this has been hard coded for the hack, taken form gridCreation
+        for (i = 0; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+		
+		// create a new div HTML element for each grid square and make it the right size
+		var square = document.createElement("div");
+		gameBoardContainer.appendChild(square);
+
+                // give each div element a unique id based on its row and column, like "s00"
+		square.id = Number(String(i) + String(j)) ;	
+
+            
+		// set each grid square's coordinates: multiples of the current row or column number
+		var topPosition = j * squareSize;
+		var leftPosition = i * squareSize;			
+		
+		// use CSS absolute positioning to place each grid square on the page
+		square.style.top = topPosition + 'px';
+		square.style.left = leftPosition + 'px';						
+	}
+}
+
+        //this.grid; //this contains locations of all of the players ships (and if they have been hit)
+       //AI.movelist; //This contains everymove the AI is yet to make.
        
+       // Abby to do:
+       // loop through AI.movelist and for i and j change colour
+       // once this has been done similar code can be used at MVP for adding ship
        
        //just realised my OO is crappy and have put in get functions, dont think js specifically needs them however
        
--- HTML5Application/public_html/sinkingships.js
+++ HTML5Application/public_html/sinkingships.js
@@ -9,16 +9,33 @@ var AI = new AI(10,10);
 
 g1 = new Grid(10,10);
 
+player.drawGrid();
+
+
 //Needs a ship creation for each of the ships styles with the positioning set below. They will all be horizontally positioned at this point
 s1 = new Ship('destroyer', 1);
 s2 = new Ship('Battleship', 1);
 
 //first index = x pos, second index = y pos, third index = if the position has been shot
-s1.setLocations([[2,2,0], [1,2,0]]);
+s1.setLocations([[8,8,0], [7,8,0]]);
 s2.setLocations([[3,0,0], [3,1,0], [3,2,0], [3, 3, 0], [3,4,0]]);
+
 
 g1.addShip(s1);
 g1.addShip(s2);
+
+// These will need adding into the addShip function later
+// also have functionality to add ship on click
+//place s1 on grid
+document.getElementById(88).style.background="black";
+document.getElementById(78).style.background="black";
+
+//place s2 on grid
+document.getElementById(30).style.background="black";
+document.getElementById(31).style.background="black";
+document.getElementById(32).style.background="black";
+document.getElementById(33).style.background="black";
+document.getElementById(34).style.background="black";
 
 console.log(g1);
 console.dir(g1);
