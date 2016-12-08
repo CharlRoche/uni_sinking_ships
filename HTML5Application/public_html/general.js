b0852b709603efa16bf9c7f876757abfa947b194
Abby Walker <walkera3@aston.ac.uk>
2016-12-08 11:59:27 +0000

    Merge origin/master into HEAD Removed HEAD msgs left in Conflicts: 	HTML5Application/public_html/instructions.html 	HTML5Application/public_html/welcome.html

--- HTML5Application/public_html/general.js
+++ HTML5Application/public_html/general.js
@@ -56,13 +56,11 @@ function jq(myid) {
 
     return "#" + myid.replace(/(:|\.|\[|\]|,|=)/g, "\\$1");
 
-}
-<<<<<<< HEAD
-;
-=======
+};
+
 
 function setHalfVolume() {
     var myAudio = document.getElementById("audio1");  
     myAudio.volume = 0.5; //Changed this to 0.5 or 50% volume since the function is called Set Half Volume ;)
 }
->>>>>>> Matt
+
