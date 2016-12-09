8e2a571a60f044ad9a57978f72c136088d455012
Charlotte Roche <>
2016-12-09 19:53:59 +0000

    Ships can be placed

--- HTML5Application/public_html/player.js
+++ HTML5Application/public_html/player.js
@@ -3,7 +3,15 @@
  *  *Contribution Log: Name/Date/Description
  *  Peter Henderson/27/10/2016/Added grid, build move list and buildmovelist function
  */
+function getCookie(name) {
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length == 2) return parts.pop().split(";").shift();
+}
 
+        var shipsPos = getCookie('shipLocation');
+        var placeships = JSON.parse(shipsPos);
+        
 
 function player(name, xSize, ySize) {
     this.name = name;
@@ -43,7 +51,7 @@ player.prototype = {
         /*PlayerShip1 = new ship('Carrier', 1);
         PlayerShip1.setLocations([[2, 5, 0], [2, 4, 0], [2, 3, 0], [2, 2, 0], [2, 1, 0]]);
         this.grid.addShip(PlayerShip1);*/
-        
+
         PlayerShip1 = new ship('Carrier', 1);
         PlayerShip1.setLocations([[placeships.carX, placeships.carY, 0], [placeships.carX, placeships.carY+1, 0], [placeships.carX, placeships.carY+2, 0], [placeships.carX, placeships.carY+3, 0], [placeships.carX, placeships.carY+4, 0]]);
         this.grid.addShip(PlayerShip1);
@@ -103,33 +111,35 @@ player.prototype = {
     },
     drawGrid: function () { //draw the players grid containing their ships
 
+        document.getElementById("p"+placeships.carX+","+placeships.carY).style.background = "green";
         //place on board Carrier
-        document.getElementById("p2,5").style.background = "green";
-        document.getElementById("p2,4").style.background = "green";
-        document.getElementById("p2,3").style.background = "green";
-        document.getElementById("p2,2").style.background = "green";
-        document.getElementById("p2,1").style.background = "green";
+        document.getElementById("p"+placeships.carX+","+(placeships.carY + 4)).style.background = "green";
+        document.getElementById("p"+placeships.carX+","+(placeships.carY + 3)).style.background = "green";
+        document.getElementById("p"+placeships.carX+","+(placeships.carY + 2)).style.background = "green";
+        document.getElementById("p"+placeships.carX+","+(placeships.carY + 1)).style.background = "green";
+        document.getElementById("p"+placeships.carX+","+placeships.carY).style.background = "green";
 
         //Batteship
-        document.getElementById("p6,3").style.background = "green";
-        document.getElementById("p7,3").style.background = "green";
-        document.getElementById("p8,3").style.background = "green";
-        document.getElementById("p9,3").style.background = "green";
+        document.getElementById("p"+placeships.batX+","+placeships.batY).style.background = "green";
+        document.getElementById("p"+(placeships.batX+1)+","+placeships.batY).style.background = "green";
+        document.getElementById("p"+(placeships.batX+2)+","+placeships.batY).style.background = "green";
+        document.getElementById("p"+(placeships.batX+3)+","+placeships.batY).style.background = "green";
 
 
         //Cruiser
-        document.getElementById("p7,5").style.background = "green";
-        document.getElementById("p7,6").style.background = "green";
-        document.getElementById("p7,7").style.background = "green";
+        document.getElementById("p"+(placeships.cruX)+","+placeships.cruY).style.background = "green";
+        document.getElementById("p"+(placeships.cruX)+","+(placeships.cruY+1)).style.background = "green";
+        document.getElementById("p"+(placeships.cruX)+","+(placeships.cruY+2)).style.background = "green";
 
         //Submarine
-        document.getElementById("p9,7").style.background = "green";
-        document.getElementById("p9,8").style.background = "green";
-        document.getElementById("p9,9").style.background = "green";
+        document.getElementById("p"+(placeships.subX)+","+(placeships.subY)).style.background = "green";
+        document.getElementById("p"+(placeships.subX)+","+(placeships.subY+1)).style.background = "green";
+        document.getElementById("p"+(placeships.subX)+","+(placeships.subY+2)).style.background = "green";
 
         //Destroyer
-        document.getElementById("p1,8").style.background = "green";
-        document.getElementById("p2,8").style.background = "green";
+        document.getElementById("p"+(placeships.desX)+","+(placeships.desY)).style.background = "green";
+        document.getElementById("p"+(placeships.desX+1)+","+(placeships.desY)).style.background = "green";
+        
     }
 }
 
